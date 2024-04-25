a5067e7c0b8c4b54ea0e197f55040235b91d8693
Asrin Yagmur <asrin.yagmur@checkbox.com.tr>
2024-04-25 18:54:20 +0300

    global colors-config, landing page

--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
 const Wrapper = ({
@@ -6,6 +7,17 @@ const Wrapper = ({
 }: {
   className?: string;
   children: ReactNode;
-}) => {};
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default Wrapper;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import Wrapper from "./components/Wrapper";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Wrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-8 overflow-hidden rounded-full border border-gray-200 bg-white px-6 py-4 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">
+          Reina is Public !!!
+        </p>
+      </div>
+      <h1 className="max-w-4xl font-bold text-5xl  md:text-6xl lg:text-7xl">
+        Dosyanızla saniyeler içerisinde
+        <span className="text-blue-600"> sohbet </span>edin
+      </h1>
+      <p className="mt-7 max-w-prose text-zinc-700 sm:text-lg">
+        Reina yükleyeceğiniz herhangi bir PDF dosyası ile sohbet etmenizi
+        sağlar. Hemen bir PDF dosyası yükleyin ve deneyin.
+      </p>
+    </Wrapper>
+  );
 }
