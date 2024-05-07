7423815f0df52a7054bbb07ca4698ff9d3be08bb
Asrin Yagmur <auyagmur@hotmail.com>
2024-05-07 17:58:10 +0300

    landing page finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,6 +81,75 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* Feature section */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Hemen konuşmaya başla
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              PDF dosyalarınızla konuşmak hiç bu kadar kolay olmamıştı
+            </p>
+          </div>
+        </div>
+
+        {/* steps */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Adım 1</span>
+              <span className="text-xl font-semibold">Kayıt olun</span>
+              <span className="mt-2 text-zinc-700">
+                Ücretsiz planla başlayın veya daha fazla özellik için{" "}
+                <Link
+                  href="/pricing"
+                  className="text-blue-700 underline underline-offset-2"
+                >
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Adım 2</span>
+              <span className="text-xl font-semibold">
+                PDF dosyanızı yükleyin
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Dosyanızı hazır hale getireceğiz
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600">Adım 3</span>
+              <span className="text-xl font-semibold">
+                Soru sormaya başlayın
+              </span>
+              <span className="mt-2 text-zinc-700">Gerçekten çok kolay</span>
+            </div>
+          </li>
+        </ol>
+
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image
+                src="/file-upload-preview.jpg"
+                alt="uploading preview"
+                width={1419}
+                height={732}
+                quality={100}
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
