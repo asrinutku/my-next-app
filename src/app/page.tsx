e64916a83eb4fc2b268bd450f7b170c9f51ece20
Asrin Yagmur <auyagmur@hotmail.com>
2024-05-21 17:56:25 +0300

    trpc init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Wrapper from "./components/Wrapper";
+import Wrapper from "../components/Wrapper";
 
 import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
